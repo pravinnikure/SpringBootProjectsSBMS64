392505dde38b94641c7b58621ad330732d356aa9
pravinjenkins <pravinnikure300@gmail.com>
2023-08-25 14:03:08 +0530

    Initial Commit

---
+++ src/main/java/com/app/restapi/SbmsRestJunitMokitoExApplication.java
@@ -0,0 +1,13 @@
+package com.app.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsRestJunitMokitoExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsRestJunitMokitoExApplication.class, args);
+	}
+
+}
